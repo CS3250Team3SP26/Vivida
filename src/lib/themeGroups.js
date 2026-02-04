1c462ae1d88bb3bbde4120cf49c76ac58e01bf8c
-CUTIE-WasTaken <sh195728@gmail.com>
2026-02-04 15:47:40 -0700

    polished it up a luh bit, now checks if theme is in group before adding, checks if theme is in group before removing, they return booleans, added ThemeCount method and hasTheme Method

--- src/lib/themeGroups.js
+++ src/lib/themeGroups.js
@@ -14,30 +14,85 @@
  */
 class ThemeGroup {
     /**
-     * 
+     * create new theme group instanxe
      * @param {string} name - display name for the group
      * @param {Array<string>} themes - list of themes in the group
+     * @throws {TypeError} - if the name is not a string or themes are not an array
+     * 
      */
     constructor(name, themes = []) {
+        if (typeof name !== 'string') {
+            throw new TypeError('ThemeGroup name must be a string');
+        }
+        if (!Array.isArray(themes)) {
+            throw new TypeError('Themes must be an array');
+        }
+
         this.name = name;
-        this.themes = themes;
+        this.themes = [...themes];
     }// constructor
 
     /**
      * Adds a theme to the group
      * @param {string} theme - The theme to add
+     * @returns {boolean} - true if theme is added, false if theme already exists
+     * @throws {TypeError} - if the theme is not a string
+     * 
      */
     addTheme(theme) {
+        if (typeof theme !== 'string') {
+            throw new TypeError('Theme must be a string');
+        }
+
+        if (this.hasTheme(theme)) {
+            return false; // Theme already exists
+        }
+
         this.themes.push(theme);
+
     }// addTheme
 
     /**
      * Removes a theme from the group
      * @param {string} theme - The theme to remove
+     * @returns {boolean} - True if theme has been removes, false if theme is not found
+     * @throws {TypeError} - if the theme is not a string
      */
     removeTheme(theme) {
+        if (typeof theme !== 'string') {
+            throw new TypeError('Theme must be a string');
+        }
+
+        if (this.hasTheme(theme) === false) {
+            return false; // Theme not found
+        }
+
         this.themes = this.themes.filter(t => t !== theme);
     }// removeTheme
+
+    /**
+     * Returns the number of themes in the group
+     * @returns {number} - count of themes
+     */
+    themeCount() {
+        return this.themes.length;
+    }
+
+    /**
+     * checks if a theme exists in the group
+     * @param {string} theme - The theme to check for
+     * @returns {boolean} - true if theme exists, otherwise it falsw
+     * @throws {TypeError} - if the theme is not a string
+     */
+    hasTheme(theme) {
+        if (typeof theme !== 'string') {
+            throw new TypeError('Theme must be a string');
+        }
+        
+        return this.themes.includes(theme);
+    }
+
+ 
 }// ThemeGroup
 
 // Export for use in other modules
