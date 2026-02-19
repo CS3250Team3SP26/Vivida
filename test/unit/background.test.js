37faea7f6d05a15f56a3d494837a0114b4d5a902
IfaGirmae <girmaiefa@icloud.com>
2026-02-19 11:18:07 -0700

    finished the installed themes

--- test/unit/background.test.js
+++ test/unit/background.test.js
@@ -24,6 +24,7 @@ jest.unstable_mockModule('../../src/lib/storageServiceWrapper.js', () => ({
 }));
 
 // Imports must come AFTER jest.unstable_mockModule
+//Initilize Test Begin: Test for Empty Groups
 const { initialize, handleMessage, getInstalledThemes } = await import('../../src/background/background.js');
 
 
@@ -43,4 +44,48 @@ describe('initialize', () => {
         // 3. Assert
         expect(mockStorageServiceWrapper.saveGroups).toHaveBeenCalledWith({});
     });
+});
+//Initilize Test: Existing Groups
+it('should NOT call saveGroups when groups already exist', async () => {
+    // 1. Arrange
+    mockStorageServiceWrapper.loadGroups.mockResolvedValue({"Dark Mode": []});
+
+    // 2. Act
+    await initialize();
+
+    // 3. Assert
+    expect(mockStorageServiceWrapper.saveGroups).not.toHaveBeenCalled({});
+});
+//Get Installed Themes Test : Filtering out ONLY themes
+// Uses an array because of multiple different kinds of themes we might have.
+describe('getInstalledThemes', () => {
+    it('should return only themes from getAll results', async () => {
+        // 1. Arrange
+        // mock browser.management.getAll to return a mixed array
+        browser.management.getAll.mockResolvedValue([
+            { id: "1", name: "Dark Theme", type: "theme" },
+            { id: "2", name: "uBlock Origin", type: "extension" }
+        ])
+        
+        // 2. Act
+        // call getInstalledThemes() and stores in const results
+        const result = await getInstalledThemes();
+        
+        // 3. Assert
+        // check that only the theme came back
+        expect(result).toEqual([{ id: "1", name: "Dark Theme", type: "theme" }]);
+        /**this whole test filters between mutliple themes and checks if it is a theme or extension
+         * if it is a theme it will keep it if not it will filter it out silently
+        **/
+    });
+});
+//Test for fails
+it('should throw when browser.management.getAll fails', async () => {
+    // 1. Arrange
+    // told jest to getALL failing with an 'API error'
+    browser.management.getAll.mockRejectedValue(new Error('API Error'));
+
+    // 2. Assert
+    // verified that getInstalledThemes catches the error and re-throws it with a message
+    await expect(getInstalledThemes()).rejects.toThrow('Failed to get installed themes');
 });
